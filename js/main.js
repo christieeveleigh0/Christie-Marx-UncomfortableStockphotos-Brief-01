c4f795b03b98e64f6466ba1fbe441bc493159f84
Christie <christieeveleigh0@gmail.com>
2018-08-08 14:44:21 +0200

    All the stuffs

---
+++ js/main.js
@@ -0,0 +1,163 @@
+// This function makes each image triger a different animation apon click. The class of each animation is 'animated' and then the corresponing one behind it. 
+
+$('.image1').on('click',function(){
+    $('.image1').addClass('animated hinge');
+});
+
+$('.image2').on('click',function(){
+    $('.image2').addClass('animated bounce');
+});
+
+$('.image3').on('click',function(){
+    $('.image3').addClass('animated flash');
+});
+
+$('.image4').on('click',function(){
+    $('.image4').addClass('animated rubberBand');
+});
+
+$('.image5').on('click',function(){
+    $('.image5').addClass('animated hinge');
+});
+
+$('.image6').on('click',function(){
+    $('.image6').addClass('animated shake');
+});
+
+$('.image7').on('click',function(){
+    $('.image7').addClass('animated swing');
+});
+
+$('.image8').on('click',function(){
+    $('.image8').addClass('animated hinge');
+});
+
+$('.image9').on('click',function(){
+    $('.image9').addClass('animated tada');
+});
+
+$('.image10').on('click',function(){
+    $('.image10').addClass('animated headShake');
+});
+
+$('.image11').on('click',function(){
+    $('.image11').addClass('animated bounceOutLeft');
+});
+
+$('.image12').on('click',function(){
+    $('.image12').addClass('animated wobble');
+});
+
+$('.image13').on('click',function(){
+    $('.image13').addClass('animated jello');
+});
+
+$('.image14').on('click',function(){
+    $('.image14').addClass('animated heartBeat');
+});
+
+$('.image15').on('click',function(){
+    $('.image15').addClass('animated hinge');
+});
+$('.image16').on('click',function(){
+    $('.image16').addClass('animated bounceIn');
+});
+$('.image17').on('click',function(){
+    $('.image17').addClass('animated hinge');
+});
+$('.image18').on('click',function(){
+    $('.image18').addClass('animated bounceInUp');
+});
+
+$('.image19').on('click',function(){
+    $('.image19').addClass('animated slideOutRight');
+});
+
+$('.image20').on('click',function(){
+    $('.image20').addClass('animated bounceInLeft');
+});
+
+$('.image21').on('click',function(){
+    $('.image21').addClass('animated slideOutLeft');
+});
+
+$('.image22').on('click',function(){
+    $('.image22').addClass('animated shake');
+});
+
+$('.image23').on('click',function(){
+    $('.image23').addClass('animated bounceInDown');
+});
+
+
+$('.image24').on('click',function(){
+    $('.image24').addClass('animated hinge');
+});
+
+$('.image25').on('click',function(){
+    $('.image25').addClass('animated slideOutDown');
+});
+
+$('.image26').on('click',function(){
+    $('.image26').addClass('animated hinge');
+});
+
+$('.image27').on('click',function(){
+    $('.image27').addClass('animated slideInRight');
+});
+
+$('.image28').on('click',function(){
+    $('.image28').addClass('animated zoomOutRight');
+});
+
+$('.image29').on('click',function(){
+    $('.image29').addClass('animated bounceOutDown');
+});
+
+$('.image30').on('click',function(){
+    $('.image30').addClass('animated fadeInUp');
+});
+
+$('.image31').on('click',function(){
+    $('.image31').addClass('animated bounce');
+});
+
+$('.image32').on('click',function(){
+    $('.image32').addClass('animated fadeInDownBig');
+});
+
+$('.image33').on('click',function(){
+    $('.image33').addClass('animated slideOutUp');
+});
+
+$('.image34').on('click',function(){
+    $('.image34').addClass('animated fadeOut');
+});
+$('.image35').on('click',function(){
+    $('.image35').addClass('animated bounceInRight');
+});
+$('.image36').on('click',function(){
+    $('.image36').addClass('animated fadeInLeft');
+});
+$('.image37').on('click',function(){
+    $('.image37').addClass('animated fadeInDown');
+});
+$('.image38').on('click',function(){
+    $('.image38').addClass('animated hinge');
+});
+$('.image39').on('click',function(){
+    $('.image39').addClass('animated bounceOut');
+});
+$('.image40').on('click',function(){
+    $('.image40').addClass('animated bounceInRight');
+});
+
+
+
+
+
+
+
+
+
+
